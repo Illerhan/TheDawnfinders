8a2e5f20a8de2fcc87f82aac47feb717d67bd601
Alexis Dupire <adupire4@gmail.com>
2025-10-07 13:50:58 +0200

    [add] Session Subsystem

---
+++ Source/TheDawnfinders/Private/SessionManagerSubsystem.cpp
@@ -0,0 +1,222 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SessionManagerSubsystem.h"
+
+void USessionManagerSubsystem::Initialize(FSubsystemCollectionBase& collection)
+{
+
+	Super::Initialize(collection);
+	IOnlineSubsystem* OnlineSubSystem = IOnlineSubsystem::Get();
+	if (OnlineSubSystem)
+	{
+		SessionInterface = OnlineSubSystem->GetSessionInterface();
+
+		if (SessionInterface.IsValid())
+		{
+			SessionInterface->OnCreateSessionCompleteDelegates.AddUObject(this, &USessionManagerSubsystem::OnCreateSessionComplete);
+			SessionInterface->OnDestroySessionCompleteDelegates.AddUObject(this, &USessionManagerSubsystem::OnCreateSessionComplete);
+			SessionInterface->OnFindSessionsCompleteDelegates.AddUObject(this,&USessionManagerSubsystem::OnFindSessionsComplete);
+			SessionInterface->OnJoinSessionCompleteDelegates.AddUObject(this,&USessionManagerSubsystem::OnJoinSessionComplete);
+			
+		}
+	}
+}
+
+void USessionManagerSubsystem::Deinitialize()
+{
+	if (SessionInterface.IsValid())
+	{
+		SessionInterface->OnCreateSessionCompleteDelegates.RemoveAll(this);
+		SessionInterface->OnDestroySessionCompleteDelegates.RemoveAll(this);
+		SessionInterface->OnFindSessionsCompleteDelegates.RemoveAll(this);
+		SessionInterface->OnJoinSessionCompleteDelegates.RemoveAll(this);
+	}
+	Super::Deinitialize();
+}
+
+void USessionManagerSubsystem::CreateAdvancesSession(int32 PublicConnection, int32 PrivateConnection, bool bUseLan, bool bAllowInvites, bool bIsDedicatedServer, bool bIsLobbiesIfAvailable)
+{
+	if (!SessionInterface.IsValid())
+	{
+		UE_LOG(LogTemp, Error, TEXT("SessionInterface is Invalid"));
+		OnSessionCreated.Broadcast(false);
+		return;
+	}
+
+	auto ExistingSession = SessionInterface->GetNamedSession(TEXT("Session"));
+	if (ExistingSession != nullptr)
+	{
+		bCreateSessionOnDestroy = true;
+		SessionInterface->DestroySession(NAME_GameSession);
+		return;
+	}
+
+	TSharedPtr<FOnlineSessionSettings> SessionSettings = MakeShareable(new FOnlineSessionSettings());
+
+	SessionSettings->NumPublicConnections = PublicConnection;
+	SessionSettings->NumPrivateConnections = PrivateConnection;
+	SessionSettings->bIsLANMatch = bUseLan;
+	SessionSettings->bShouldAdvertise = true;
+	SessionSettings->bAllowJoinInProgress = true;
+	SessionSettings->bAllowInvites = bAllowInvites;
+	SessionSettings->bUsesPresence = true;
+	SessionSettings->bUseLobbiesIfAvailable = bIsLobbiesIfAvailable;
+	SessionSettings->bIsDedicated = bIsDedicatedServer;
+	SessionSettings->bAllowJoinViaPresence = true;
+	SessionSettings->bAllowJoinViaPresenceFriendsOnly = false;
+
+	const ULocalPlayer* LocalPlayer = GetWorld()->GetFirstLocalPlayerFromController();
+
+	if (!LocalPlayer)
+	{
+		UE_LOG(LogTemp, Error, TEXT("LocalPlayer is invalid"));
+		OnSessionCreated.Broadcast(false);
+	}
+
+	bool bSuccess = SessionInterface->CreateSession(*LocalPlayer->GetPreferredUniqueNetId(), NAME_GameSession, *SessionSettings);
+    
+	if (!bSuccess)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to create session!"));
+		OnSessionCreated.Broadcast(false);
+	}
+}
+
+void USessionManagerSubsystem::FindAdvancedSession(int32 MaxResults, bool bUseLan)
+{
+	if (!SessionInterface.IsValid())
+	{
+		UE_LOG(LogTemp, Error, TEXT("SessionInterface is Invalid"));
+		OnSessionCreated.Broadcast(false);
+		return;
+	}
+
+	SessionSearch = MakeShareable(new FOnlineSessionSearch());
+	SessionSearch->MaxSearchResults = MaxResults;
+	SessionSearch->bIsLanQuery = bUseLan;
+
+	const ULocalPlayer* LocalPlayer = GetWorld()->GetFirstLocalPlayerFromController();
+	if (!LocalPlayer)
+	{
+		UE_LOG(LogTemp, Error, TEXT("LocalPlayer is invalid"));
+		OnSessionCreated.Broadcast(false);
+		return;
+	}
+
+	bool bSuccess = SessionInterface->FindSessions(*LocalPlayer->GetPreferredUniqueNetId(),SessionSearch.ToSharedRef());
+	if (!bSuccess)
+	{
+		UE_LOG(LogTemp, Error, TEXT("SessionSearch is invalid"));
+		OnSessionJoined.Broadcast(false);
+	}
+}
+
+void USessionManagerSubsystem::JoinAdvancedSession(TArray<FBlueprintSessionResult> SessionResults)
+{
+	if (!SessionInterface.IsValid())
+	{
+		UE_LOG(LogTemp, Error, TEXT("SessionInterface is Invalid"));
+		OnSessionJoined.Broadcast(false);
+		return;
+	}
+
+	const ULocalPlayer* LocalPlayer = GetWorld()->GetFirstLocalPlayerFromController();
+	if (!LocalPlayer)
+	{
+		UE_LOG(LogTemp, Error, TEXT("LocalPlayer is invalid"));
+		OnSessionJoined.Broadcast(false);
+		return;
+	}
+
+	bool bSuccess = SessionInterface->JoinSession(*LocalPlayer->GetPreferredUniqueNetId(),NAME_GameSession,SessionResults[0].OnlineResult);
+	if (!bSuccess)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to join session!"));
+		OnSessionJoined.Broadcast(false);
+	}
+}
+void USessionManagerSubsystem::DestroySession()
+{
+	if (!SessionInterface.IsValid())
+	{
+		UE_LOG(LogTemp, Error, TEXT("SessionInterface is Invalid"));
+		return;
+	}
+	SessionInterface->DestroySession(NAME_GameSession);
+}
+
+void USessionManagerSubsystem::OnCreateSessionComplete(FName SessionName, bool bWasSuccessful)
+{
+	if (bWasSuccessful)
+	{
+		UE_LOG(LogTemp, Log, TEXT("Session created successfully"));
+		// Logique vers le changement de map a mettre ici ( redirection vers le lobby)
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to create session!"));
+	}
+}
+
+void USessionManagerSubsystem::OnSessionDestroyComplete(FName SessionName, bool bWasSuccessful)
+{
+	if (bWasSuccessful)
+	{
+		UE_LOG(LogTemp, Log, TEXT("Session destroy successfully"));
+		if (bCreateSessionOnDestroy)
+		{
+			bCreateSessionOnDestroy = false;
+			CreateAdvancesSession();
+		}
+	}else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to destroy session!"));
+	}
+}
+
+void USessionManagerSubsystem::OnFindSessionsComplete(bool bWasSuccessful)
+{
+	if (!bWasSuccessful || !SessionSearch.IsValid())
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to find sessions!"));
+		OnSessionsFound.Broadcast(TArray<FBlueprintSessionResult>(), false);
+		return;
+	}
+
+	UE_LOG(LogTemp, Log, TEXT("Found sessions!"));
+
+	TArray<FBlueprintSessionResult> Sessions;
+	for (const auto& SearchResult : SessionSearch->SearchResults)
+	{
+		FBlueprintSessionResult BPResult;
+		BPResult.OnlineResult = SearchResult;
+		Sessions.Add(BPResult);
+	}
+
+	OnSessionsFound.Broadcast(Sessions,true);
+}
+
+void USessionManagerSubsystem::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+	if (Result != EOnJoinSessionCompleteResult::Success)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to join session!"));
+		OnSessionJoined.Broadcast(false);
+		return;
+	}
+
+	UE_LOG(LogTemp, Log, TEXT("Join session complete"));
+
+	FString ConnectInfo;
+	if (SessionInterface->GetResolvedConnectString(NAME_GameSession, ConnectInfo))
+	{
+		UE_LOG(LogTemp, Log, TEXT("Connecting to server: %s"), *ConnectInfo);
+
+		APlayerController* PC = GetWorld()->GetFirstPlayerController();
+		if (PC)
+			PC->ClientTravel(ConnectInfo,ETravelType::TRAVEL_Absolute);
+	}
+
+	OnSessionJoined.Broadcast(true);
+}
---
+++ Source/TheDawnfinders/Private/SessionManagerSubsystem.h
@@ -0,0 +1,76 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "FindSessionsCallbackProxyAdvanced.h"
+#include "SessionManagerSubsystem.generated.h"
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSessionCreated, bool, bWasSuccesful);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSessionJoined, bool, bWasSuccesful);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnSessionsFound,const TArray<FBlueprintSessionResult>&, Results,bool, bWasSuccesfull);
+/**
+ * 
+ */
+UCLASS()
+class THEDAWNFINDERS_API USessionManagerSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+
+	UFUNCTION(BlueprintCallable, Category = "SessionManager")
+	void CreateAdvancesSession(
+		int32 PublicConnection = 4,
+		int32 PrivateConnection = 0,
+		bool bUseLan = false,
+		bool bAllowInvites = true,
+		bool bIsDedicatedServer = false,
+		bool bIsLobbiesIfAvailable = true
+		);
+
+	UFUNCTION(BlueprintCallable, Category = "SessionManager")
+	void FindAdvancedSession(
+		int32 MaxResults = 20,
+		bool bUseLan = false
+		);
+
+	UFUNCTION(BlueprintCallable, Category = "SessionManager")
+	void JoinAdvancedSession(TArray<FBlueprintSessionResult> SessionResults);
+
+	UFUNCTION(BlueprintCallable, Category = "SessionManager")
+	void DestroySession();
+
+	UPROPERTY(BlueprintAssignable, Category = "SessionManager")
+	FOnSessionCreated OnSessionCreated;
+
+	UPROPERTY(BlueprintAssignable, Category = "SessionManager")
+	FOnSessionsFound OnSessionsFound;
+
+	UPROPERTY(BlueprintAssignable, Category = "SessionManager")
+	FOnSessionJoined OnSessionJoined;
+
+private:
+	void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+	void OnSessionDestroyComplete(FName SessionName, bool bWasSuccessful);
+	void OnFindSessionsComplete(bool bWasSuccessful);
+	void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+
+	FDelegateHandle OnCreateSessionCompleteDelegateHandle;
+	FDelegateHandle OnSessionDestroyCompleteDelegateHandle;
+	FDelegateHandle OnFindSessionCompleteDelegateHandle;
+	FDelegateHandle OnJoinSessionCompleteDelegateHandle;
+
+	IOnlineSessionPtr SessionInterface;
+
+	TSharedPtr<class FOnlineSessionSearch> SessionSearch;
+
+	TSharedPtr<class FOnlineSessionSettings> PendingSessionSettings;
+	bool bCreateSessionOnDestroy = false;
+	
+};
