ac8b4845769fac9dfe62e747eff713cde5ed9437
Arthur <arthur.perigno@gmail.com>
2025-10-13 12:29:12 +0200

    [Update] Inventory System Progress

---
+++ Source/TheDawnfinders/Public/GameFramework/CustomHUD.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "CustomHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEDAWNFINDERS_API ACustomHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public :
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UUserWidget* MainWidget;
+};
---
+++ Source/TheDawnfinders/Public/Widgets/UMainWidget.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UMainWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEDAWNFINDERS_API UMainWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
