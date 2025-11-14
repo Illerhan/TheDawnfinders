c240f984101f121ed397ac34b15cecc74856566c
Arthur <arthur.perigno@gmail.com>
2025-11-14 14:35:27 +0100

    [Add] Enemy Base

---
+++ Source/TheDawnfinders/Private/Actors/Enemy/ABaseEnemy.cpp
@@ -0,0 +1,21 @@
+#include "Actors/Enemy/ABaseEnemy.h"
+
+
+ABaseEnemy::ABaseEnemy()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ABaseEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ABaseEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
