c2c6417e1a09add9478a16b058569adc1cec20eb
Arthur <arthur.perigno@gmail.com>
2025-12-03 15:23:18 +0100

    [Add] Start Interact Text

---
+++ Source/TheDawnfinders/Private/Widgets/UWorldInteractibleWidget.cpp
@@ -0,0 +1,12 @@
+#include "Widgets/UWorldInteractibleWidget.h"
+
+
+void UWorldInteractibleWidget::DisplayText_Implementation(const FString& Text)
+{
+
+}
+
+void UWorldInteractibleWidget::HideText_Implementation()
+{
+
+}
