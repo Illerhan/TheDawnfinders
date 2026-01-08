4cbdf75c3b8aa86dbfcf1ca7fdc3e10966b02eef
Alexis Dupire <adupire4@gmail.com>
2026-01-08 10:28:39 +0100

    [fix] Crash on playmode end

--- Source/TheDawnfinders/Private/DebugSubsyteme.cpp
+++ Source/TheDawnfinders/Private/DebugSubsyteme.cpp
@@ -8,6 +8,21 @@ void UDebugSubsyteme::Initialize(FSubsystemCollectionBase& Collection)
 {
 	Super::Initialize(Collection);
 	UE_LOG(LogTemp, Warning, TEXT("--- DebugSubsyteme Initialized ---"));
-	DebugWindow = MakeShareable(new FDebugWindow());
+	if (!DebugWindow.IsValid())
+	{
+		DebugWindow = MakeShareable(new FDebugWindow());
+	}
 	
+}
+void UDebugSubsyteme::Deinitialize()
+{
+	// C'est ici qu'on évite le crash. On détruit la fenêtre AVANT que le monde ne disparaisse.
+	if (DebugWindow.IsValid())
+	{
+		// Si ta classe FDebugWindow a une méthode genre .Close() ou .Destroy(), appelle-la ici.
+		// Sinon, le simple fait de reset le pointeur devrait suffire si le SlateIMWindowBase est bien fait.
+		DebugWindow.Reset(); 
+	}
+
+	Super::Deinitialize();
 }
