15031bfce968a68945bac6b8474bc20ea32cf469
Arthur <arthur.perigno@gmail.com>
2026-02-04 11:08:49 +0100

    [Add] New Spike Trap Behavior

--- Source/TheDawnfinders/Private/Actors/Traps/ASpikeTrap.h
+++ Source/TheDawnfinders/Private/Actors/Traps/ASpikeTrap.h
@@ -17,4 +17,10 @@ public:
 
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
 	void DoSpikeAnim();
+
+
+// === SPIKE TRAP PARAMETERS ===
+protected :
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Spike Trap")
+	float SpikeOutDuration = 0.5f;
 };
