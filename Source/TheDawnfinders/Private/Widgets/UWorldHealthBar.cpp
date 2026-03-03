9611ff7a89b657f2cde76b376574af866d1c6b1d
Arthur <arthur.perigno@gmail.com>
2026-03-03 16:55:17 +0100

    [Update] Collider Player + [Add] Setup World Health Bar

---
+++ Source/TheDawnfinders/Private/Widgets/UWorldHealthBar.cpp
@@ -0,0 +1,13 @@
+#include "Widgets/UWorldHealthBar.h"
+
+void UWorldHealthBar::Setup_Implementation(int Parts) {
+
+}
+
+void UWorldHealthBar::TakeDamage_Implementation(float NewPercent) {
+
+}
+
+void UWorldHealthBar::Heal_Implementation(float NewPercent) {
+
+}
---
+++ Source/TheDawnfinders/Public/Widgets/UWorldHealthBar.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UWorldHealthBar.generated.h"
+
+UCLASS()
+class THEDAWNFINDERS_API UWorldHealthBar : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public :
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void Setup(int Parts);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void TakeDamage(float NewPercent);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    void Heal(float NewPercent);
+
+
+protected :
+	UPROPERTY(BlueprintReadWrite)
+	int PartCount;
+
+	UPROPERTY(BlueprintReadWrite)
+	bool bHealthBarDisplayed;
+
+	UPROPERTY(BlueprintReadWrite)
+	float CurrentProgress;
+};
