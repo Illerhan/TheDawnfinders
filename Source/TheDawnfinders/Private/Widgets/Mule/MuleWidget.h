e2007d04a12f7173bc996e12e28084d1cf5cb424
Alexis Dupire <adupire4@gmail.com>
2026-03-16 14:09:03 +0100

    [add] Mule

---
+++ Source/TheDawnfinders/Private/Widgets/Mule/MuleWidget.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MuleWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEDAWNFINDERS_API UMuleWidget : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintNativeEvent, Category = Mule)
+	void UpdateMuleWidget(int Charges, float ChargesCooldown, float CallCooldown);
+};
