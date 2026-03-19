0b1f8c54fbb81bbe52999e85284a7e9faf40d5bb
Arthur <arthur.perigno@gmail.com>
2026-03-19 12:26:25 +0100

    [Fix] Camera Jittering + [Fix] Ammos when slot moved

--- Source/TheDawnfinders/Private/Components/UInventoryComponent.cpp
+++ Source/TheDawnfinders/Private/Components/UInventoryComponent.cpp
@@ -436,6 +436,7 @@ void UInventoryComponent::SortByCategories()
 		InventorySlots[i].CurrentInfos.ItemData = SortedInventory[i].CurrentInfos.ItemData;
 		InventorySlots[i].Quantity = SortedInventory[i].Quantity;
 		InventorySlots[i].CurrentInfos.Durability = SortedInventory[i].CurrentInfos.Durability;
+		InventorySlots[i].CurrentInfos.AmmoInMagazine = SortedInventory[i].CurrentInfos.AmmoInMagazine;
 	}
 }
 
@@ -498,6 +499,8 @@ void UInventoryComponent::SortItems()
 	for (int i = 0; i < SortedInventory.Num(); i++) {
 		InventorySlots[i].CurrentInfos.ItemData = SortedInventory[i].CurrentInfos.ItemData;
 		InventorySlots[i].Quantity = SortedInventory[i].Quantity;
+		InventorySlots[i].CurrentInfos.Durability = SortedInventory[i].CurrentInfos.Durability;
+		InventorySlots[i].CurrentInfos.AmmoInMagazine = SortedInventory[i].CurrentInfos.AmmoInMagazine;
 	}
 
 	for (int i = SortedInventory.Num(); i < InventorySlots.Num(); i++) {
