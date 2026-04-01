6bba69adbd301e79b3a1d3a14d777a0c84422edf
Arthur <arthur.perigno@gmail.com>
2026-04-01 15:36:46 +0200

    [Add] Setup Widget Recap

---
+++ Source/TheDawnfinders/Public/Widgets/UGameRecapWidget.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UGameRecapWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEDAWNFINDERS_API UGameRecapWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public :
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void DisplaySuccessRecap();
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void DisplayFailRecap();
+};
