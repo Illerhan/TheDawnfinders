44b7b72987cc97be8c2fa264619cb4bbaba021e6
Alexis Dupire <adupire4@gmail.com>
2026-04-23 11:06:11 +0200

    [add] Incremental doors

---
+++ Source/TheDawnfinders/Private/Actors/MovableObjects/IncrementalDoor.h
@@ -0,0 +1,33 @@
+﻿ #pragma once
+  
+  
+  #include "CoreMinimal.h"
+  
+  #include "Actors/MovableObjects/Doors.h"
+  
+  #include "IncrementalDoor.generated.h"
+  
+  
+  UCLASS()
+  
+  class THEDAWNFINDERS_API AIncrementalDoor : public ADoors
+  {
+      GENERATED_BODY()
+
+  public:
+      AIncrementalDoor();
+
+  protected:
+      virtual void BeginPlay() override;
+      virtual void Tick(float DeltaTime) override;
+
+      // Intercepte l'appel du Lever
+      virtual void StartOpening() override;
+      virtual void StopOpening() override;
+      virtual void StopMainAction_Implementation() override;
+
+  private:
+      int32 ActivationCount = 0;
+      float TargetCurvePos  = 0.f;
+      bool  bMovingToStep   = false;
+  };
