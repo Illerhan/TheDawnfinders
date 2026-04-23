c2898639bd36d5c2713a133f1493e6015fec4ff7
Arthur <arthur.perigno@gmail.com>
2026-04-23 17:05:00 +0200

    [Setup] Menu Top Bar widget

---
+++ Source/TheDawnfinders/Private/Widgets/UTopBarWidget.cpp
@@ -0,0 +1,11 @@
+#include "Widgets/UTopBarWidget.h"
+
+void UTopBarWidget::ActualiseSelectedMenu_Implementation(int CurrentMenuIndex)
+{
+
+}
+
+void UTopBarWidget::ActualiseGoldCount_Implementation(int GoldCount)
+{
+
+}
