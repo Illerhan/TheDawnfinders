d9b0f31af2fdbd8a4cb201e4c9b76b69f0be1497
Arthur <arthur.perigno@gmail.com>
2026-04-30 14:19:02 +0200

    [Fix] Client death while placing landmine / aiming / reloading

--- Source/TheDawnfinders/Private/Actors/Player/APlayerCharacter.cpp
+++ Source/TheDawnfinders/Private/Actors/Player/APlayerCharacter.cpp
@@ -253,6 +253,8 @@ void AAPlayerCharacter::HideProgress_Implementation()
 
     if (!PlayerWidget->GetProgressBar()) return;
 
+    UE_LOG(LogTemp, Display, TEXT("STOOOOOP"));
+
     UWorldProgressBar* ProgressBar = PlayerWidget->GetProgressBar();
     ProgressBar->Hide();
 }
@@ -365,7 +367,11 @@ void AAPlayerCharacter::Multicast_SetCurrentPlayerState_Implementation(EPlayerSt
         break;
 
     case EPlayerState::Fallen:
-
+        StopAutoLock();
+        ItemComponent->StopAim();
+        ItemComponent->CancelReload();
+        ItemComponent->StopMainAction();
+        StopAutoMoveCharacter(true);
         StopMovementForDuration(4.5f);
         break;
 
