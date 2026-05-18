77f6831c087fa586cd59f1416147bb4194ace66d
Arthur <arthur.perigno@gmail.com>
2026-05-18 10:23:24 +0200

    [Fix] Project doesn't launch

--- Source/TheDawnfinders/Private/Components/UHealthComponent.cpp
+++ Source/TheDawnfinders/Private/Components/UHealthComponent.cpp
@@ -157,7 +157,7 @@ void UHealthComponent::TakeDamage(float quantity, EVFXType VFXType)
 			IPlayerInterface::Execute_DoDamagePostProcess(GetOwner(), 1.f);
 		}
 
-		IPlayerInterface::Execute_PlayVibration(GetOwner(), EVibrationType::StrongLong);
+		IPlayerInterface::Execute_PlayVibration(GetOwner(), EVibrationType::StrongLong, 0);
 
 		DamageSoundID = UAkGameplayStatics::PostEvent(DamageSound,GetOwner(),0,FOnAkPostEventCallback(), false);
 	}
