4dc9e84f3be5a79aedb5bdd3ceaa6030619b703f
Alexis Dupire <adupire4@gmail.com>
2026-05-18 15:12:39 +0200

    [fix] Music manager crashes + Extract music placeholder

--- Source/TheDawnfinders/Private/Actors/SoundManagement/MusicManager.cpp
+++ Source/TheDawnfinders/Private/Actors/SoundManagement/MusicManager.cpp
@@ -78,7 +78,9 @@ void UMusicManager::ApplyState(EMusicState NewState)
     switch (NewState)
     {
     case EMusicState::ZoneCalm:   SoundToPlay = CurrentZone->MusicCalmSound;   break;
-    case EMusicState::ZoneCombat: SoundToPlay = CurrentZone->MusicCombatSounds[FMath::RandRange(0, CurrentZone->MusicCombatSounds.Num()-1)]; break;
+    case EMusicState::ZoneCombat: 
+        if(CurrentZone->MusicCombatSounds.IsEmpty() )return; 
+        SoundToPlay = CurrentZone->MusicCombatSounds[FMath::RandRange(0, CurrentZone->MusicCombatSounds.Num()-1)]; break;
     case EMusicState::Extraction: SoundToPlay = ExtractionSound; break;
     default: return;
     }
